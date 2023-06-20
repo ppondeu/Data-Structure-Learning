7e60e0fb1067c88220caff87782a448f19b14b49
Nitcharat <pondaeorz008y@gmail.com>
2023-06-21 02:55:58 +0700

    feat add bubble sort

--- Sorting-Algorithm/basic_sort.c
+++ Sorting-Algorithm/basic_sort.c
@@ -39,12 +39,33 @@ void insertionSort(int arr[], int n){
     }
 }
 
+void bubbleSort(int arr[], int n){
+    int flag;
+    for(int i=0; i<n-1; i++){
+        flag = 0;
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(&arr[j], &arr[j+1]);
+                flag = 1;
+            }
+        }
+        if(flag == 0){
+            break;
+        }
+    }
+}
+
 int main(){
     int arr[] = {5, 4, 3, 2, 1};
     int n = sizeof(arr)/sizeof(arr[0]);
     selectionSort(arr, n);
     printArray(arr, n);
+
     insertionSort(arr, n);
     printArray(arr, n);
+
+    bubbleSort(arr, n);
+    printArray(arr, n);
+    
     return 0;
 }
