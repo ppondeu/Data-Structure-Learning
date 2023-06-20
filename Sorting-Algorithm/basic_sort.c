a03cec12c576c8b0a8c585f2dfc4bf90deb92419
Nitcharat <pondaeorz008y@gmail.com>
2023-06-21 02:52:43 +0700

    feat add insertion sort

--- Sorting-Algorithm/basic_sort.c
+++ Sorting-Algorithm/basic_sort.c
@@ -26,6 +26,19 @@ void selectionSort(int arr[], int n){
     }
 }
 
+void insertionSort(int arr[], int n){
+    int key, j;
+    for(int i=1; i<n; i++){
+        key = arr[i];
+        j = i-1;
+        while(j>=0 && arr[j]>key){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+}
+
 int main(){
     int arr[] = {5, 4, 3, 2, 1};
     int n = sizeof(arr)/sizeof(arr[0]);
